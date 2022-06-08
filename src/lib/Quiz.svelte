43247f5f44828153c44772e4a2c28019ccbae485
Matilda Mared <matildamared@live.se>
2022-06-08 21:51:06 +0200

    implement super simple quiz component

---
+++ src/lib/Quiz.svelte
@@ -0,0 +1,19 @@
+<script>
+	let result = "";
+	let correctAnswer = "B";
+
+	function pickAnswer(answer) {
+		if (answer === correctAnswer) {
+			return (result = "Correct!");
+		}
+		result = "Oops.. That was wrong!";
+	}
+</script>
+
+<div>
+	<h4>Result: {result}</h4>
+	<button on:click={() => pickAnswer("A")}>Answer A</button>
+	<button on:click={() => pickAnswer("B")}>Answer B</button>
+	<button on:click={() => pickAnswer("C")}>Answer C</button>
+	<button on:click={() => pickAnswer("D")}>Answer D</button>
+</div>
