7781e3f6b21ee03d835b7e99be3bd01a5532f627
Matilda Mared <matildamared@live.se>
2022-06-10 20:20:37 +0200

    add functionality to be able to see if the answer was correct or not

--- src/lib/Question.svelte
+++ src/lib/Question.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
 	import type { Question } from "../models/Question";
 	export let question: Question;
+	let isCorrect;
+	let isAnswered = false;
 
-	let answers = question.incorrect_answers;
+	interface Answer {
+		answer: string;
+		isCorrect: boolean;
+	}
+
+	let answers: Answer[] = question.incorrect_answers.map((answer) => {
+		return {
+			answer: answer,
+			isCorrect: false,
+		};
+	});
+
+	let allAnswers: Answer[] = shuffle([
+		...answers,
+		{ answer: question.correct_answer, isCorrect: true },
+	]);
+
+	function shuffle(array: Answer[]) {
+		const shuffledArray = [...array];
+		return shuffledArray.sort(() => Math.random() - 0.5);
+	}
+
+	function checkQuestion(boolean) {
+		isAnswered = true;
+		isCorrect = boolean;
+	}
 </script>
 
 <h3>{@html question.question}</h3>
 
-{#each answers as answer}
-	<button>{@html answer}</button>
+{#if isAnswered}
+	<h4>
+		{#if isCorrect}
+			You got it right! 🥳
+		{:else}
+			You goofed up... 😨
+		{/if}
+	</h4>
+{/if}
+
+{#each allAnswers as answer}
+	<button on:click={() => checkQuestion(answer.isCorrect)}
+		>{@html answer.answer}</button
+	>
 {/each}
