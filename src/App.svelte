9a6939a937537f5ca156828df49b38078605a5c9
Matilda Mared <matildamared@live.se>
2022-06-11 06:40:40 +0200

    remove unused styles

--- src/App.svelte
+++ src/App.svelte
@@ -3,43 +3,9 @@
 </script>
 
 <main>
-	<h1>Hello Typescript!</h1>
+	<h1>Computer Quiz 👩‍💻</h1>
 	<Quiz />
 </main>
 
 <style>
-	:global(h4) {
-		color: green;
-	}
-
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-			Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 2rem auto;
-		max-width: 14rem;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
 </style>
